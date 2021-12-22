38676bbb8bc63f298d7a7c871cf7e7cf7d085294
dr-eduardovasconcelos <56181935+dr-eduardovasconcelos@users.noreply.github.com>
2021-12-21 23:44:24 -0300

    Update enhanced_countingsort.h

--- enhanced_countingsort.h
+++ enhanced_countingsort.h
@@ -6,11 +6,11 @@
 /*
 * These functions are able to sort negative integers
 */
-void couting_sort(long **in_array, long len_array);
-void couting_sort_min_max(long **in_array, long len_array, long min_value, long max_value);
+void e_couting_sort(long **in_array, long len_array);
+void e_couting_sort_min_max(long **in_array, long len_array, long min_value, long max_value);
 
 /*
 * These functions are only able to sort integers greater than 0
 */
-void couting_sort_naturalnumbers(long **in_array, long len_array);
-void couting_sort_naturalnumbers_max(long **in_array, long len_array, long max_value);
+void e_couting_sort_naturalnumbers(long **in_array, long len_array);
+void e_couting_sort_naturalnumbers_max(long **in_array, long len_array, long max_value);
